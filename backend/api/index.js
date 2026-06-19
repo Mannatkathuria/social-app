ec82cbbaaf8a915b4efce28bebef11ababc884c3
Mannat <mannatkathuria23@gmail.com>
2026-06-20 02:15:16 +0530

    restore api

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,16 @@
-export default function handler(req, res) {
-    res.status(200).json({
-        success: true,
-        message: "Hello from Vercel"
-    });
+import serverless from "serverless-http";
+import app from "../src/app.js";
+import connectDB from "../src/db/index.js";
+
+let isConnected = false;
+
+async function handler(req, res) {
+    if (!isConnected) {
+        await connectDB();
+        isConnected = true;
+    }
+
+    return serverless(app)(req, res);
 }
+
+export default handler;
